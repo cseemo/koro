b887b71c8aeda4840e3bbb32ee49c81d6163664b
Marin Nelson <junkmar@gmail.com>
2014-08-08 11:29:43 -0700

    Removed bootstrap theme from meanjs stack Moved the css from our theme into the enviromental variables and removed it from the layout so that it is dynamically added Username is now displayed on the top right of the screen Disabled viewing pages without being logged in (this needs more work)

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,10 +1,13 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', '$location',
+	function($scope, Authentication, Menus, $location) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+
+		if( ! Authentication.user ) $location.path('/signin');
+		console.log('Auth: ', $scope.authentication);
 
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
