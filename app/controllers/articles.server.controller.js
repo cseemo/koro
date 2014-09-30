4628c77881794cbc027756b70b5d51ba96189ad5
Admin <admin@jonathan-johnsons-MacBook-Pro.local>
2014-09-30 02:38:25 -0700

    t

--- app/controllers/articles.server.controller.js
+++ app/controllers/articles.server.controller.js
@@ -45,7 +45,7 @@ exports.create = function(req, res) {
 			});
 		} else {
 			var socketio = req.app.get('socketio');
-			socketio.sockets.emit('article.created', article);
+			socketio.sockets.emit('article.created', 'Hey there');
 			res.jsonp(article);
 		}
 	});
