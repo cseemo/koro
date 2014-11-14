6af19bc182c4587703e5b2552dd64309fc8f3f9d
Admin <admin@jonathan-johnsons-MacBook-Pro.local>
2014-11-13 22:52:47 -0700

    Removed Shops and Workorders menu

--- public/modules/workorders/config/workorders.client.config.js
+++ public/modules/workorders/config/workorders.client.config.js
@@ -4,8 +4,8 @@
 angular.module('workorders').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Workorders', 'workorders', 'dropdown', '/workorders(/create)?', 'fa fa-wrench');
-		Menus.addSubMenuItem('topbar', 'workorders', 'List Workorders', 'workorders');
-		Menus.addSubMenuItem('topbar', 'workorders', 'New Workorder', 'workorders/create');
+		// Menus.addMenuItem('topbar', 'Workorders', 'workorders', 'dropdown', '/workorders(/create)?', 'fa fa-wrench');
+		// Menus.addSubMenuItem('topbar', 'workorders', 'List Workorders', 'workorders');
+		// Menus.addSubMenuItem('topbar', 'workorders', 'New Workorder', 'workorders/create');
 	}
 ]);
