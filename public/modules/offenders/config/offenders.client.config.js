2a919da4f0f57f6553c01986bff09ae91b4c8343
Admin <admin@jonathan-johnsons-MacBook-Pro.local>
2014-12-01 16:30:02 -0700

    Changed navigation functions for better on ipad

--- public/modules/offenders/config/offenders.client.config.js
+++ public/modules/offenders/config/offenders.client.config.js
@@ -4,8 +4,8 @@
 angular.module('offenders').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Offenders', 'offenders', 'dropdown', '/offenders(/create)?', 'fa fa-male');
-		Menus.addSubMenuItem('topbar', 'offenders', 'List Offenders', 'offenders');
-		Menus.addSubMenuItem('topbar', 'offenders', 'New Offender', 'offenders/create');
+		// Menus.addMenuItem('topbar', 'Offenders', 'offenders', 'dropdown', '/offenders(/create)?', 'fa fa-male');
+		// Menus.addSubMenuItem('topbar', 'offenders', 'List Offenders', 'offenders');
+		// Menus.addSubMenuItem('topbar', 'offenders', 'New Offender', 'offenders/create');
 	}
 ]);
