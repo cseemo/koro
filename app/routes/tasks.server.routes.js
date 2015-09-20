cc79cbf2d181f58a4800ca63d7c5ad36edade337
Five Cs <fivecsconsulting@gmail.com>
2015-09-19 17:12:20 -0700

    asf

--- app/routes/tasks.server.routes.js
+++ app/routes/tasks.server.routes.js
@@ -14,31 +14,7 @@ module.exports = function(app) {
 
 	app.route('/rejectTask')
 		.post(users.requiresLogin, tasks.rejectTask);
-		
-
-	app.route('/testPhone')
-		.get(tasks.testPhone)
-		.post(tasks.testPhone);
-
-	app.route('/testSMS')
-		.get(tasks.testSMS)
-		.post(tasks.testSMS);
-
-	app.route('/myPhone')
-		.get(tasks.myPhone)
-		.post(tasks.myPhone);
-
-	app.route('/respondToPhone')
-		.get(tasks.respondToPhone)
-		.post(tasks.respondToPhone);
-		
-	app.route('/phoneUpdate')
-		.get(tasks.phoneUpdate)
-		.post(tasks.phoneUpdate);
-
-
-		
-
+	
 
 	app.route('/tasks/:taskId')
 		.get(tasks.read)
