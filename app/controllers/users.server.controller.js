4361238a46d71123a73603ab1f3b6dc8d741382b
Five Cs <fivecsconsulting@gmail.com>
2016-01-06 00:20:01 -0700

    asdf

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -28,7 +28,7 @@ var testMe = function(){
 	console.log('test me called..');
 server = new ftpd.FtpServer(options.host, {
   getInitialCwd: function() {
-    return '/share';
+    return '/';
   },
   getRoot: function() {
     return process.cwd();
