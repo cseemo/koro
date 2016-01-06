c097486054822501be2fd7cadfaebd2f752b1b6b
Five Cs <fivecsconsulting@gmail.com>
2016-01-05 22:28:47 -0700

    adsf

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -21,7 +21,7 @@ var ftpd = require('ftp-server')
 var testMe = function(){
 	console.log('test me called..');
 	// Path to your FTP root 
-ftpd.fsOptions.root = '../opt'
+ftpd.fsOptions.root = '/ftp'
 // Start listening on port 21 (you need to be root for ports < 1024) 
 ftpd.listen(3331)
 
